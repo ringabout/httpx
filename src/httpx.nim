0f290a06e70166fc4807ec062a9ce7746f5b86c7
unknown <crc32@qq.com>
2022-11-02 00:04:19 +0800

    fix #14

--- src/httpx.nim
+++ src/httpx.nim
@@ -356,7 +356,7 @@ proc processEvents(selector: Selector[Data],
             let lastError = osLastError()
 
             when usePosixVersion:
-              if lastError.int32 in {EWOULDBLOCK, EAGAIN}:
+              if lastError.int32 in [EWOULDBLOCK, EAGAIN]:
                 break
             else:
               if lastError.int == WSAEWOULDBLOCK:
